8886e32532a5e56a466a1915d6f270e899d51838
gamesmith <840014140@qq.com>
2021-05-30 14:17:15 +0800

    添加了OffsetFollower

---
+++ Assets/SLibrary/OffsetFollower.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+namespace SLibrary
+{
+    public class OffsetFollower : MonoBehaviour
+    {
+        public Vector3 Offset;
+        private Transform _target;
+
+        public Transform Target
+        {
+            set
+            {
+                _target = value;
+                Update();
+            }
+        }
+
+        private void Update()
+        {
+            if (ReferenceEquals(_target, null))
+            {
+                return;
+            }
+
+            transform.position = _target.position + Offset;
+        }
+    }
+}
---
+++ Assets/SLibrary/StaticUtil.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+namespace SLibrary
+{
+    public class StaticUtil
+    {
+        public static T GetOrAddComponent<T>(GameObject go) where T : Component
+        {
+            if (ReferenceEquals(go, null))
+            {
+                return null;
+            }
+
+            var ret = go.GetComponent<T>();
+            if (ret == null)
+            {
+                ret = go.AddComponent<T>();
+            }
+
+            return ret;
+        }
+        
+        public static EventTrigger AttachEventTriggers(GameObject go, UnityAction<BaseEventData> onClick = null, UnityAction<BaseEventData> onBeginDrag = null,
+            UnityAction<BaseEventData> onDrag = null, UnityAction<BaseEventData> onEndDrag = null,UnityAction<BaseEventData> onDrop = null)
+        {
+            // Profiler.BeginSample("AttachEventTrigger");
+            var trigger = GetOrAddComponent<EventTrigger>(go);
+            trigger.triggers.Clear();
+            EventTrigger.Entry entry = new EventTrigger.Entry();
+            entry.eventID = EventTriggerType.PointerClick;
+            entry.callback.AddListener((data) => { onClick?.Invoke((PointerEventData) data); });
+            trigger.triggers.Add(entry);
+
+            entry = new EventTrigger.Entry();
+            entry.eventID = EventTriggerType.BeginDrag;
+            entry.callback.AddListener((data) => { onBeginDrag?.Invoke((PointerEventData) data); });
+            trigger.triggers.Add(entry);
+
+            entry = new EventTrigger.Entry();
+            entry.eventID = EventTriggerType.Drag;
+            entry.callback.AddListener((data) => { onDrag?.Invoke((PointerEventData) data); });
+            trigger.triggers.Add(entry);
+
+            entry = new EventTrigger.Entry();
+            entry.eventID = EventTriggerType.EndDrag;
+            entry.callback.AddListener((data) => { onEndDrag?.Invoke((PointerEventData) data); });
+            trigger.triggers.Add(entry);
+
+            entry = new EventTrigger.Entry();
+            entry.eventID = EventTriggerType.Drop;
+            entry.callback.AddListener((data) => { onDrop?.Invoke((PointerEventData) data); });
+            trigger.triggers.Add(entry);
+            // Profiler.EndSample();
+            return trigger;
+        }
+    }
+}
