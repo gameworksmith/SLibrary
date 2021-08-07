f756fb9c23b7dbc37c34639aebdd4261dbd06ce4
gamesmith <840014140@qq.com>
2021-08-07 18:01:33 +0800

    添加了qr的工具和阿里云oss的工具

---
+++ Assets/SLibrary/ThirdUtil/AliyunOSS/Scripts/AliyunOSSUploader.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using Aliyun.OSS;
+using Aliyun.OSS.Common;
+using UnityEngine;
+
+public class AliyunOSSUploader : MonoBehaviour
+{
+    [Header("存储节点")]
+    public string endPoint;
+    [Header("站点")]
+    public string host;
+    [Header("oss的key")]
+    public string key;
+    [Header("oss的secret")]
+    public string secret;
+    // Start is called before the first frame update
+
+    public static AliyunOSSUploader Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public BucketInfo GetBucketInfo(string bucketName)
+    {
+        OssClient client = new OssClient(endPoint, key, secret);
+        return client.GetBucketInfo(bucketName);
+    }
+
+    public void Upload(byte[] data, string bucketName, string root, string fileName, System.Action<string> onComplete)
+    {
+        
+        OssClient client = new OssClient(endPoint, key, secret);
+        
+        try {
+            
+            using (Stream stream = new MemoryStream(data))
+            {
+                string saveKey = root + "/" + fileName;
+                var result = client.PutObject(bucketName, saveKey, stream);
+                if (result.HttpStatusCode == HttpStatusCode.OK)
+                {
+                    string url = host + "/" + root + "/" + fileName;
+                    onComplete?.Invoke(url);
+                    Debug.Log("上传成功");
+                }
+                else
+                {
+                    Debug.Log("上传失败");
+                    onComplete?.Invoke(null);                   
+                }
+            }
+        }
+        catch (OssException e) {
+            Debug.Log("字符串数据上传错误："+ e);
+        }
+        catch (Exception e)
+        {
+            Debug.Log("字符串数据上传错误：" + e);
+        }
+
+    }
+
+}
---
+++ Assets/SLibrary/ThirdUtil/QR/QRCodeUtil.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using ZXing;
+
+namespace QRCode
+{
+    public static class QRCodeUtil
+    {
+        /// <summary>
+        /// 将制定字符串信息转换成二维码图片信息
+        /// </summary>
+        /// <param name="formatStr">要产生二维码的字符串信息</param>
+        /// <param name="width">二维码的宽度</param>
+        /// <param name="height">二维码的高度</param>
+        /// <returns></returns>
+        public static Color32[] GeneQRCode(string formatStr, int width, int height)
+        {
+            ZXing.QrCode.QrCodeEncodingOptions options = new ZXing.QrCode.QrCodeEncodingOptions(); //绘制二维码之前 进行设置
+
+            options.CharacterSet = "UTF-8"; //设置字符编码，确保字符串信息保持正确
+
+            options.Width = width; //设置二维码宽
+            options.Height = height; //设置二维码高
+            options.Margin = 1; //设置二维码留白 （值越大，留白越大，二维码越小）
+
+            BarcodeWriter writer = new BarcodeWriter {Format = BarcodeFormat.QR_CODE, Options = options}; //实例化字符串绘制二维码工具
+
+            return writer.Write(formatStr);
+        }
+
+
+        /// <summary>
+        /// 根据二维码图片信息绘制制定字符串信息的二维码到指定区域
+        /// </summary>
+        /// <param name="str">字符串信息</param>
+        /// <param name="width">二维码的宽度</param>
+        /// <param name="height">二维码的高度</param>
+        /// <returns></returns>
+        public static Texture2D ShowQRCode(string str, int width, int height)
+        {
+            Texture2D texture = new Texture2D(width, height);
+
+            Color32[] colors = GeneQRCode(str, width, height);
+
+            texture.SetPixels32(colors);
+
+            texture.Apply();
+
+            return texture;
+        }
+    }
+}
